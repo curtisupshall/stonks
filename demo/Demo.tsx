e9bdda58168a16d621616e419f2c7afd4c9a52a5
Curtis Upshall <curtisupshall@uvic.ca>
2019-07-06 19:21:18 -0700

    Initialize demo

---
+++ demo/Demo.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+class Demo extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        )
+    }
+}
+
+export default Demo
